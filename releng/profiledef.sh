157b864115ae159458e87c1bc01dced920b63ddd
jdfthetech <jdf@onlinemarketspecialist.com>
2021-02-13 20:44:17 -0800

    initial commit - all releng items

---
+++ releng/profiledef.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+
+iso_name="T430Arch"
+iso_label="T430ARCH_$(date +%Y%m)"
+iso_publisher="JDFTheTech <https://github.com/jdfthetech>"
+iso_application="Arch Linux Live/Rescue CD for the T430"
+
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/copyfiles.sh"]="0:0:755"
+  ["/usr/local/bin/setfont.sh"]="0:0:755"
+)
